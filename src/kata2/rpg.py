0eedafe1967d348a28b34ab8d6b01c4becaa37a1
Blai <blaimunyoz@gmail.com>
2020-05-16 18:24:40 +0200

    kata1

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,7 +4,7 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","v","w","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+    caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
     for i in range(passLen):
         if i == 0:
             contra = random.choice(caracteres)
