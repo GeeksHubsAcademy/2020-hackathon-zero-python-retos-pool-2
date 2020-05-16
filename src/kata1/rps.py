19d8d3daf87fb3fe30ee1e6b1c2b2692c7c5174e
Blai <blaimunyoz@gmail.com>
2020-05-16 17:44:03 +0200

    test 1 and 2 done

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,45 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    if ai == "piedra" and player == "papel":
+        return "Ganaste!"
+    elif ai == "piedra" and player == "piedra":
+        return "Empate!"
+    elif ai == "piedra" and player == "tijeras":
+        return "Perdiste!"
+    elif ai == "papel" and player == "papel":
+        return "Empate!"
+    elif ai == "papel" and player == "piedra":
+        return "Perdiste!"
+    elif ai == "papel" and player == "tijeras":
+        return "Ganaste!"
+    elif ai == "tijeras" and player == "papel":
+        return "Perdiste!"
+    elif ai == "tijeras" and player == "piedra":
+        return "Ganaste!"
+    elif ai == "tijeras" and player == "tijeras":
+        return "Empate!"
+
 
 # Entry Point
 def Game():
     #
     #
-    
+    print("Estás jugando a piedra, papel o tijeras.")
+    player = input("Por favor, introduce cual de los 3 movimientos quieres hacer: ")
+    player = player.lower()
+
+    suerte = randint(0,2)
+    ai = options[suerte]
+    ai = ai.lower()
+
     #
     #
-    
     winner = quienGana(player, ai)
 
     print(winner)
 
+    return 0
+
+
+Game()
--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,14 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","v","w","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+    for i in range(passLen):
+        if i == 0:
+            contra = random.choice(caracteres)
+        else:
+            contra = contra + random.choice(caracteres)
+    return contra
 
-    return ""
+longitud = int(input("longitud contraseña a crear: "))
+contra = RandomPasswordGenerator(longitud)
+print(contra)
