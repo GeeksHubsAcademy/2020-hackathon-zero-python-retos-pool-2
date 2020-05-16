d98b73e5e7ed81e4050c18f2bf293ccb3d2d16d9
Miguel Angel <mimaca359@gmail.com>
2020-05-16 23:41:53 +0200

    Terminando

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -1,8 +1,7 @@
-import random
+import random 
 from random import randint
 
 options = ["Piedra", "Papel", "Tijeras"]
-print("Bienvenido al piedra, papel, tijera.")
 
 # El resultado de salida son las siguientes String
 #'Empate!'
@@ -12,28 +11,41 @@ def quienGana(player, ai):
     player = player.lower()
     ai = ai.lower()
     if(player == ai):
-        return "Empate!"
-    elif((player=='piedra' and ai == 'tijeras') or (player == 'tijeras' and ai == 'papel') or (player == 'papel' and ai == 'piedra')):
-        return "Ganaste!"
+        respuesta = 'Empate!'
+    elif((player == 'tijeras' and ai == 'papel') or (player == 'papel' and ai == 'piedra') or (player == 'piedra' and ai == 'tijeras')):
+        respuesta = 'Ganaste!'
     else:
-        return "Perdiste!"
+        respuesta = 'Perdiste!'
+
+    return respuesta
 
 # Entry Point
 def Game():
+    #Se podría meter también en un try-except por si el valor introducido no es numérico, aunque en este caso no se usará
     try:
-        condicion = False
-        while condicion == False:
-            opcion = int(input("Escoge una opción numérica: \n1-Piedra / 2-Papel / 3-Tijeras: "))
-            if(opcion in (1,2,3)):
-                    opcion = int(opcion) - 1
-                    condicion = True
+        #variable de condicion para el bucle
+        seleccionValida = False
+        #Se pregunta la elección hasta que se introduce un valor numérico válido
+        while seleccionValida == False:
+            seleccion = int(input("1-Piedra / 2-Papel / 3-Tijeras\nSelecciona una opción numérica: "))
+            if(seleccion in (1,2,3)):
+                seleccion = seleccion - 1
+                seleccionValida = True
             else:
-                print("Selección no válida, vuelve a intentarlo")
+                print("Selección no válida, vuelve a intentarlo.\n")    
 
-        ai = random.randint(0,2)
-        print('La maquina escoge ' + options[ai])
-        winner = quienGana(options[int(opcion)], options[ai])
+        #se define la variable que recogerá el random de la ia
+        iaSeleccion = random.randint(0,2)
+
+        seleccion = options[seleccion]
+        iaSeleccion = options[iaSeleccion]
+
+        #Se llama al método que devuelve quien gana
+        winner = quienGana(seleccion, iaSeleccion)
+        print("Has seleccionado: " + seleccion + "\nLa mágina ha seleccionado: " + iaSeleccion)
         print(winner)
+
     except:
         print("Valor introducido no válido")
+
 Game()
--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -6,49 +6,49 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger(_name_)
 
 
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    update.message.reply_text("Hola, soy un bot")
-
+    return "Hola, Geeks!"
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    update.message.reply_text("¿En que puedo ayudarte?")
+
+    return "Ayudame!"
 
 def mayus(update, context):
-    """Envia el mensaje en mayusculas"""
-    update.message.reply_text(update.message.text.upper())
+    #se recoge el mensaje escrito  
+    text = str(context.args[0])
+    text = text.upper()
+    return text
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    update.message.reply_text(update.message.text[::-1])
-    
+    #se recoge el mensaje
+    return update.message.text[::-1]
+    #También funciona con --> ''.join(reversed(texto))
 
 def error(update, context):
     """Envia los errores por consola"""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    """Inicio del Bot"""
-    #iniciamos el updater/instanciamos
+    '''Inicio del Bot'''
+    #Colocamos el Token creado por FatherBot
     updater = Updater(token=open("./bot_token").read(), use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
     dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
     dp.add_handler(MessageHandler(Filters.text, alreves))
     
     # Y este espera al error
@@ -60,5 +60,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
